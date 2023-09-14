7db5bd3145c2b39a8659ba9cefac7a269e06215e
songhee24 <akaratel61@gmail.com>
2023-09-15 01:00:52 +0600

    trying to setup ts

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = (env) => {
     return {
         mode: env.mode ?? 'development',
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: '[name].[contenthash].js',
