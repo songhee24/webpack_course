e5e33861a100ed82310cbe2d6a2cf1d69adb2280
songhee24 <akaratel61@gmail.com>
2023-09-18 19:37:57 +0600

    now i only can use scss logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import './App.css'
+import './App.scss'
 import './test.scss'
 const App = () => {
     const [counter, setCounter] = useState(0)
--- webpack.config.ts
+++ webpack.config.ts
@@ -29,8 +29,15 @@ export default (env: EnvVariables) => {
         module: {
             rules: [
                 {
-                    test: /\.css$/i,
-                    use: ["style-loader", "css-loader"],
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
                 },
                 {
                     // ts-laoder умееть работать с JSX из под коробки
