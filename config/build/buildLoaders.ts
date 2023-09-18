e031eb30456a6549fedaeb2d91f8e549d5960746
songhee24 <akaratel61@gmail.com>
2023-09-18 20:19:24 +0600

    update logic of scss loader and ts laoder

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,26 +5,26 @@ import {BuildOptions} from "./types/types";
 export const buildLoaders = (options: BuildOptions): ModuleOptions['rules'] => {
     const isDev = options.mode === 'development'
 
-    const scssLoader =         {
-            test: /\.s[ac]ss$/i,
-            use: [
-                // Creates `style` nodes from JS strings
-                // "style-loader",
-                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                // Translates CSS into CommonJS
-                "css-loader",
-                // Compiles Sass to CSS
-                "sass-loader",
-            ],
-        };
+    const scssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            // "style-loader",
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
 
-    const tsLoader =         {
-            // ts-laoder умееть работать с JSX из под коробки
-            // Если бы мы не использовали typescript: нужен был бы babel-loader
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        };
+    const tsLoader = {
+        // ts-laoder умееть работать с JSX из под коробки
+        // Если бы мы не использовали typescript: нужен был бы babel-loader
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
 
     return [
         scssLoader,
