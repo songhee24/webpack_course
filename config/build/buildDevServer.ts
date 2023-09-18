b30ebcc2bc020b8580a4f57ed212f28be98807d3
songhee24 <akaratel61@gmail.com>
2023-09-18 20:01:05 +0600

    build dev server helper

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,7 @@
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+export function buildDevServer(): DevServerConfiguration {
+    return {
+        port: env.port ?? 3000,
+        open: true,
+    }
+}
