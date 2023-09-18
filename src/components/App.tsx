b925b8e753d3d0918ce303869a248b7bc63054c4
songhee24 <akaratel61@gmail.com>
2023-09-18 19:22:00 +0600

    usage first react component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const App = () => {
+    const [counter, setCounter] = React.useState(0)
+
+    const increment = () => setCounter(prevState => prevState+1)
+
     return (
         <div>
-            Hello World
+            <h1>{counter}</h1>
+            <button onClick={increment}>inc</button>
         </div>
     )
 }
