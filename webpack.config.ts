ad255d108da17e15e3e2ba8c74e6113ad66ba287
songhee24 <akaratel61@gmail.com>
2023-09-19 20:33:08 +0600

    decomposing webpack config logic

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {buildWebpack} from "./config/build/buildWebpack";
+import {BuildPaths} from "./config/build/types/types";
 
 type Mode = 'development' | 'production'
 
@@ -11,56 +10,15 @@ interface EnvVariables {
 }
 
 export default (env: EnvVariables) => {
-
-    const isDev = env.mode === 'development'
-
-    const config: webpack.Configuration = {
-        mode: env.mode ?? 'development',
+    const paths: BuildPaths = {
+        output: path.resolve(__dirname, 'dist'),
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name].[contenthash].js',
-            clean: true
-        },
-        plugins: [
-            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-            !isDev && new MiniCssExtractPlugin({
-                filename: 'css/[name].[contenthash:8].css',
-                chunkFilename: 'css/[name].[contenthash:8].css'
-            })
-            // new webpack.ProgressPlugin()
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        // Creates `style` nodes from JS strings
-                        // "style-loader",
-                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        // Translates CSS into CommonJS
-                        "css-loader",
-                        // Compiles Sass to CSS
-                        "sass-loader",
-                    ],
-                },
-                {
-                    // ts-laoder умееть работать с JSX из под коробки
-                    // Если бы мы не использовали typescript: нужен был бы babel-loader
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js']
-        },
-        devtool: isDev ? 'inline-source-map' : false,
-        devServer: isDev ? {
-            port: env.port ?? 8000,
-            open: true,
-        } : undefined
-    };
-    return config
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+    
+    return buildWebpack({
+        port: env.port ?? 3000,
+        mode: env.mode ?? 'development',
+        paths: paths
+    });
 }
