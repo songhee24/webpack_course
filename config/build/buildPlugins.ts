9c0e0f796e4a05b9be06c4239073dc5c12165e7a
songhee24 <akaratel61@gmail.com>
2023-09-19 17:13:35 +0600

    added bundle analyzer plugin to check bundle size

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,16 +2,25 @@ import {Configuration} from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/types";
+import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
 
 export const buildPlugins = (options: BuildOptions):Configuration['plugins'] => {
     const isDev = options.mode === 'development'
+    const isProd = options.mode === 'production'
 
-    return [
+    const plugins: Configuration['plugins'] = [
         new HtmlWebpackPlugin({template: options.paths.html}),
-        !isDev && new MiniCssExtractPlugin({
+    ]
+
+    if(isProd) {
+        plugins.push(new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css'
-        })
-        // new webpack.ProgressPlugin()
-    ]
+        }))
+        plugins.push(new BundleAnalyzerPlugin())
+    }
+
+    if(isDev) {}
+
+    return plugins
 }
