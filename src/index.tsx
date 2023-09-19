75aa936c297d1ebe3dbdf75a9b862f50a43de099
songhee24 <akaratel61@gmail.com>
2023-09-19 17:01:42 +0600

    lazy loading suspense implement

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,26 @@ import App from "./components/App";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {AboutLazy} from "./pages/about/About.lazy";
 import {ShopLazy} from "./pages/shop/Shop.lazy";
+import {Suspense} from "react";
 
 const root = document.getElementById('root')
 
-if(!root) {
+if (!root) {
     throw new Error('root not found')
 }
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <App/>,
         children: [
             {
                 path: '/about',
-                element: <AboutLazy />
+                element: <Suspense fallback={'loading...'}><AboutLazy/></Suspense>
             },
             {
                 path: '/shop',
-                element: <ShopLazy/>
+                element: <Suspense fallback={'loading...'} ><ShopLazy/></Suspense>
             }
         ]
     },
