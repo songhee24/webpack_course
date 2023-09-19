7ece06721758a91101616247d3ed49a91f6fb846
songhee24 <akaratel61@gmail.com>
2023-09-19 14:54:00 +0600

    everything works

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import classes from './App.module.scss'
+import {Outlet} from "react-router-dom";
 const App = () => {
     const [counter, setCounter] = useState(0)
 
@@ -9,6 +10,7 @@ const App = () => {
         <div>
             <h1>{counter}</h1>
             <button className={classes.button} onClick={increment}>inc</button>
+            <Outlet />
         </div>
     )
 }
