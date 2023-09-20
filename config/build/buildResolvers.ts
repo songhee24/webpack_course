41a4b250a7603dafb714b18e9a41a187a2549a55
songhee24 <akaratel61@gmail.com>
2023-09-20 17:38:43 +0600

    alias confid done for webpack

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,6 +3,9 @@ import {BuildOptions} from "./types/types";
 
 export const buildResolvers = (options: BuildOptions): Configuration['resolve'] => {
     return  {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': options.paths.src,
+        }
     }
 }
