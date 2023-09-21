1c167baf2ad4e6cd5a1ef1dbb803c269fd3ef462
songhee24 <akaratel61@gmail.com>
2023-09-21 12:00:40 +0600

    copy plugin to past plugins from some places and use it in build folder

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,6 +6,7 @@ import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import path from "path";
+import CopyPlugin from "copy-webpack-plugin";
 
 export const buildPlugins = ({mode, paths, analyzer, platform}: BuildOptions): Configuration['plugins'] => {
     const isDev = mode === 'development'
@@ -23,6 +24,13 @@ export const buildPlugins = ({mode, paths, analyzer, platform}: BuildOptions): C
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css'
         }))
+        plugins.push(
+            new CopyPlugin({
+                patterns: [
+                    { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
+                ],
+            }),
+        )
     }
 
     if (isDev) {
